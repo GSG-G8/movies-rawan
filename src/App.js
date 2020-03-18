c247c8d80f5c258c3fdf3d9330ceceae60c89172
rawan <rawan.designer@hotmail.com>
2020-03-18 13:24:22 +0200

    Create the show movie component Relates #6

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
 import React, { Component } from 'react';
 import './App.css';
-import SearchForm from './movies'
+import SearchForm from './movies';
+import ShowMovie from './showMovie';
 
-class App extends Component{
-  state={
+class App extends Component {
+  state = {
     searchValue: null,
   };
 
-  setSearchValue= newValue=> this.setState({searchValue: newValue});
+  setSearchValue = newValue => this.setState({ searchValue: newValue });
 
-  render(){
-  return (
-    <div>
-      <header>
-      <h1 class="h1-app">Movie App</h1>
-      <h3 class="h1-app">The Place Where You Can Find All Your Favourite Movies</h3>
-      </header>
-      <SearchForm class="search-form" SearchMovie={this.setSearchValue} />
-    </div>
-  );
-}
+  render() {
+    return (
+      <div>
+        <header>
+          <h1 className="h1-app">Movie App</h1>
+          <h3 className="h1-app">
+            The Place Where You Can Find All Your Favourite Movies
+          </h3>
+        </header>
+        <SearchForm class="search-form" SearchMovie={this.setSearchValue} />
+        <ShowMovie />
+      </div>
+    );
+  }
 }
 
 export default App;
